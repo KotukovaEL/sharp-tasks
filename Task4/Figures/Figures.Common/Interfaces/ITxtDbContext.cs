fcd0b8dbe21820db4f1cc79ee99c26b0617f72c1
Lena <kotukovaea@gmail.com>
2024-03-11 19:43:41 +0400

    Add ITxDbContext

--- Task4/Figures/Figures.Common/Interfaces/ITxtDbContext.cs
+++ Task4/Figures/Figures.Common/Interfaces/ITxtDbContext.cs
@@ -5,7 +5,6 @@ namespace Figures.Common.Interfaces
     public interface ITxtDbContext<TKey, TValue>
     {
         Dictionary<TKey, TValue> EntitiesMap { get; }
-        IIdGenerator IdGenerator { get; }
         TValue GetByKey(TKey key);
         void SaveChanges();
         void ReadFile();

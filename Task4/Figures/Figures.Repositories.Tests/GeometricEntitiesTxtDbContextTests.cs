91d2bf8747b7134e2c3b3fdfded6433fcfbb68c8
Lena <kotukovaea@gmail.com>
2024-03-13 20:17:06 +0400

    Add Tests

---
+++ Task4/Figures/Figures.Repositories.Tests/GeometricEntitiesTxtDbContextTests.cs
@@ -0,0 +1,46 @@
+﻿using Figures.Model;
+using Moq;
+using System;
+using System.IO;
+using Xunit;
+
+namespace Figures.Repositories.Tests
+{
+    public class GeometricEntitiesTxtDbContextTests 
+    {
+        [Fact]
+        public void Should_save_changes_correctly()
+        {
+            using var memoryStream = new MemoryStream();
+            using var writer = new TestTextWriter(memoryStream);
+
+            var sourceIO = new Mock<ISourceIO>();
+            sourceIO
+                .Setup(x => x.CreateWriter())
+                .Returns(writer);
+
+            sourceIO
+                .Setup(x => x.ReadAllLines())
+                .Returns(new string[] {});
+
+            var context = new GeometricEntitiesTxtDbContext(sourceIO.Object);
+
+            var point = new Point(2, 2);
+            point.Id = 1;
+            context.Add(point);
+            context.SaveChanges();
+            memoryStream.Seek(0, SeekOrigin.Begin);
+
+            var results = new StreamReader(memoryStream).ReadToEnd();
+            var expectedResults = string.Join(Environment.NewLine,
+                " Id: 1",
+                " X: 2",
+                " Y: 2",
+                "- Point",
+                "",
+                "");
+
+            Assert.Equal(expectedResults, results);
+        }
+    }
+}
---
+++ Task4/Figures/Figures.Repositories.Tests/UsersRepositoryTests.cs
@@ -0,0 +1,49 @@
+﻿using Figures.Common.Interfaces;
+using Figures.Model;
+using FluentAssertions;
+using Moq;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+using Xunit;
+
+namespace Figures.Repositories.Tests
+{
+    public class UsersRepositoryTests
+    {
+        [Fact]
+        public void Should_try_add_user_correctly()
+        {
+            var calledSaveChanges = false;
+            User? userFromRepo = null;
+            var context = new Mock<ITxtDbContext<string, User>>();
+            context
+                .Setup(x => x.Add(It.IsAny<User>()))
+                .Callback((User user) => { userFromRepo = user; });
+
+            context
+                .Setup(x => x.SaveChanges())
+                .Callback(() => { calledSaveChanges = true; });
+
+            var repo = new UsersRepository(context.Object);
+            var expectedUser = new User("Name");
+            repo.TryAdd(expectedUser.Name);
+            userFromRepo.Should().BeEquivalentTo(expectedUser);
+            Assert.True(calledSaveChanges);
+        }
+
+        [Fact]
+        public void Should_get_user_correctly()
+        {
+            var userFromRepo = new User("Name");
+            var context = new Mock<ITxtDbContext<string, User>>();
+            context
+                .Setup(x => x.GetByKey(It.IsAny<string>()))
+                .Returns((string name) => { return userFromRepo; });
+
+            var repo = new UsersRepository(context.Object);
+            var expectedUser = repo.GetUser(userFromRepo.Name);
+            userFromRepo.Should().BeEquivalentTo(expectedUser);
+        }
+    }
+}

720a150e554c35bb7fe899a9797421d390226c4c
Дмитрий <dima251200@yandex.ru>
2024-04-30 22:17:02 +0400

    Add new impl

---
+++ Task5/WeakestLink/WeakestLink.Handlers/NodesImplementation/WeakestLinkGame.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeakestLink.Handlers.NodesImplementation
+{
+    public class WeakestLinkGame : IWeakestLinkGame
+    {
+        public void Run(int playersCount, int strikeoutNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
